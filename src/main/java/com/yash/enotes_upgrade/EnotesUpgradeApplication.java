a4cbf597bd0e7980b48d13acb2da90c68f5f26bd
Yash-tyagi <tyagiyash29@gmail.com>
2024-11-21 01:22:36 +0530

    initial commit

---
+++ src/main/java/com/yash/enotes_upgrade/EnotesUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.yash.enotes_upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesUpgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesUpgradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/enotes_upgrade/EnotesUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.enotes_upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesUpgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
